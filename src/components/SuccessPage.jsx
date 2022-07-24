e2d146eabc8415ecf1d3a4ffeabe2b71b09272e9
Daniel Toba <danieloloruntoba681@gmail.com>
2022-07-24 22:33:46 +0100

    Made some adjustments to thank you page

--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -9,15 +9,13 @@ const SuccessPage = () => {
       <div className="conainer mx-auto mt-3">
         <div className="flex flex-col items-center md:px-6 md:flex-row">
           <div className="flex-col px-3">
-            <h1 className="font-bold text-3xl md:text-5xl animate-pulse">
-              Thank You!
-            </h1>
+            <h1 className="font-bold text-5xl animate-pulse">Thank You!</h1>
             <p>
               Thank you for joining the waitlist. We would let you know as soon
               as we launch.
             </p>
           </div>
-          <img className="max-w-sm" src={ThankYouImg} alt="Thank You" />
+          <img className="max-w-xs" src={ThankYouImg} alt="Thank You" />
         </div>
         {/* <div className=""> */}
         {/* </div> */}
